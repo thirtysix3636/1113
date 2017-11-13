59e932f0dffa6231311089d98e7cc261d32e583a
YoonHoo <yoonhu1431@gmail.com>
2017-11-13 13:41:52 +0900

    로그인/회원가입/DB_form 구조

---
+++ app/src/main/java/com/example/a1102/dbhelper/DBHelper.java
@@ -0,0 +1,29 @@
+package com.example.a1102.dbhelper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Jeff_Hwang on 2017. 11. 13..
+ */
+/*
+
+public class DBHelper extends SQLiteOpenHelper{
+
+    public DBHelper(Context context){
+        super(context, "unknown.db", null);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
+*/
---
+++ app/src/main/java/com/example/a1102/member/MemberLogin.java
@@ -0,0 +1,18 @@
+package com.example.a1102.member;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jeff_Hwang on 2017. 11. 13..
+ */
+
+public class MemberLogin extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
